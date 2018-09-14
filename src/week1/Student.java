45204a9fc270287cf39916c53c2b898fcc7cb37c
dominhkha <38008326+dominhkha@users.noreply.github.com>
2018-09-14 14:41:22 +0700

    up

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,7 +3,10 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
 
     /**
@@ -11,6 +14,10 @@ public class Student {
      */
     Student() {
         // TODO:
+        name="student";
+        id="000";
+        group="INT22041";
+        email="uet@vnu.edu.vn";
     }
 
     /**
@@ -21,6 +28,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name=n;
+        id=sid;
+        email=em;
+        group="INT22041";
     }
 
     /**
@@ -29,10 +40,39 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        name=s.name;
+        id=s.id;
+        group=s.group;
+        email=s.email;
     }
 
     String getInfo() {
         // TODO:
+        System.out.print( "ten: "+ name+"\n"+"id: " +id+"\n"+"group: " +group+"\n"+"email: "+email);
         return null; // xóa dòng này sau khi cài đặt
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setId(String id){
+        this.id=id;
+    }
+    public void setGroup(String group){
+        this.group=group;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getName(){
+        return name;
+    }
+     public String getId(){
+        return id;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public String getEmail(){
+        return email;
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,21 +3,81 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+    private Student[] ds=new Student[100];
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return s1.getGroup().equals(s2.getGroup());
     }
 
     void studentsByGroup() {
         // TODO:
+        int i=0,j;
+        int k=0;
+        String[] tenLop=new String[100];
+        boolean[] d = new boolean[100];
+        
+        for(i=0;i<100;i++) {
+            d[i]=false;
+        }
+        i=0;
+        while(ds[i]!=null){
+            if(d[i]==false) {
+                tenLop[k]=ds[i].getGroup();
+                for(j=0;ds[j]!=null;j++){
+                    if(d[j]==false&&ds[j].getGroup().equals(tenLop[k])){
+                        d[j]=true;
+                    }
+                }
+                k++;
+               // System.out.println(k);
+            }
+            i++;
+        }
+        for(i=0;i<k;i++){
+            System.out.println("cac hoc sinh thuoc lop "+tenLop[i] +" la : ");
+            for(j=0;ds[j]!=null;j++){
+                if(ds[j].getGroup().equals(tenLop[i])) ds[j].getInfo();
+            } 
+        } 
+        
     }
 
     void removeStudent(String id) {
         // TODO:
+        int i=0;
+        for(i=0;ds[i]!=null;i++)
+            if(ds[i].getId().equals(id)) break;
+        for(int j=i;ds[j]!=null;j++){
+            ds[j]=ds[j+1];
+        }
     }
 
     public static void main(String[] args) {
         // TODO:
+        // bai 6 : 
+        Student stu=new Student("Do Minh Kha","17020827","17020827@vnu.edu.vn");
+        stu.getInfo();
+        //end
+        StudentManagement hs=new StudentManagement();
+        // kiem tra same group : 
+        /*
+        Student stu1 = new Student("Do Minh A","17020827","17020827@vnu.edu.vn");
+        Student stu2 = new Student("Do Minh B","17020827","17020827@vnu.edu.vn");
+        if(hs.sameGroup(stu1, stu2)) System.out.println(" hai sinh vien "+stu1.getName()+" va "+stu2.getName()+"cung lop");
+        else System.out.println(" hai sinh vien "+stu1.getName()+" va "+stu2.getName()+" cung lop");
+        */
+        hs.ds[0]=new Student();
+        hs.ds[1]=new Student();
+        hs.ds[2]=new Student();
+        hs.ds[3]=new Student();
+        hs.ds[3].setId("123");
+        hs.ds[0].setName("Do Minh A");
+        hs.ds[0].setGroup("N1");
+        hs.ds[1].setName("Do Minh B");
+        hs.ds[1].setGroup("N2");
+        hs.ds[2].setName("Do Minh C");
+        hs.ds[2].setGroup("N1");
+        hs.removeStudent(hs.ds[3].getId());
+        hs.studentsByGroup();
     }
 }
