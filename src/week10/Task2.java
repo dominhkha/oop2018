21ffc51113cbb70cabfd5494e9485bcf205b564f
dominhkha <38008326+dominhkha@users.noreply.github.com>
2018-11-15 22:15:18 +0700

    update

--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,46 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+    public static void main(String[] args){
+        floatNumber t = new floatNumber();
+        int n=1000;
+        Random rand = new Random();
+        for(int i=0;i<n;i++){
+            float x=rand.nextFloat();
+            t.append(x);
+        }
+        t.bubbleSort();
+        t.print();
+    }
+    
+}
+class floatNumber{
+    private float[] a;
+    private int n;
+    public floatNumber(){
+        a=new float[1000];
+        n=0;
+    }
+    public void append(float x){
+        a[n]=x;
+        n++;
+    }
+    public void bubbleSort(){
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[i]){
+                    float c=a[i];
+                    a[i]=a[j];
+                    a[j]=c;
+                }
+            }
+        }
+    }
+    public void print(){
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
 }
--- src/week9/Utils.java
+++ src/week9/Utils.java
@@ -1,4 +1,84 @@
 package week9;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Utils {
+    public static String readContentFromFile(String path) throws FileNotFoundException, IOException{
+        File file = new File(path);
+        FileInputStream input = new FileInputStream(file);
+        BufferedReader read;
+        read = new BufferedReader(new InputStreamReader(input));
+        String line  = read.readLine();
+        read.close();
+        return line;
+    }
+    public static void writeContentToFile(String path) throws FileNotFoundException, IOException{
+        File file;
+        file = new File(path);
+        FileOutputStream output = new FileOutputStream(file);
+        BufferedWriter write = new BufferedWriter(new OutputStreamWriter(output));
+        write.write("Do Minh Kha");
+        write.close();
+    }
+    public static void appendContentToFile(String path){
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        try{
+            File file = new File(path);
+            fw = new FileWriter(file.getAbsolutePath(),true);
+            bw = new BufferedWriter(fw);
+            bw.write("  University of Engineering and Technology");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        finally{
+            if(bw!=null) try {
+                
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        }
+    }
+    public static File findFileByName(String folderPath, String fileName){
+         File file = new File(folderPath);
+         if(file.exists()){
+             if(file.isFile()){
+                 System.out.println(file.getAbsolutePath());
+             }
+             else {
+                 File[] listFile =file.listFiles();
+                 int t=0;
+                 for(File f:listFile){
+                     if(f.getName().equals(fileName)){
+                         t=1;
+                         System.out.println(fileName+"is here!");
+                     }
+                 }
+                 if(t==0) System.out.println(fileName+ " is not exist in "+folderPath);
+             }
+         }
+        return null;
+    }
+    public static void main(String[] args) throws IOException{
+        String path = "src/week9/exercise.txt";
+        writeContentToFile(path);
+        System.out.println( readContentFromFile(path));
+        appendContentToFile(path);
+        System.out.println( readContentFromFile(path));
+        findFileByName("src/week9","exercise.txt");
+    }
 }
