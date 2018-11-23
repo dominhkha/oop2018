10c2e30a6a96775cce26c6f11beef80e0ad9f403
dominhkha <38008326+dominhkha@users.noreply.github.com>
2018-11-23 11:29:02 +0700

    update

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,33 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task1 {
+    public static void main(String[] args){
+        Generic<Integer> list = new Generic<Integer>();
+        for(int i=0;i<10;i++){
+            list.add(i);
+        }
+        list.print();
+    }
 }
+class Generic<T>{
+    private  ArrayList<T> array;
+    private int i=0;
+    private int NUM1=100;
+    public Generic(){
+        this.array= new ArrayList<T>();
+        
+    }
+    
+    public void add(T o){
+        this.array.add(o);
+        i++;
+    }
+    public void print(){
+        for(int j=0;j<i;j++){
+            System.out.println(this.array.get(j));
+        }
+    }
+}
+
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,39 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        int[] a = {1, 4, 2, 5, 7, 4, 10};
+        Generic2<Integer> list = new Generic2<Integer>();
+        for (int i=0;i<a.length;i++) {
+              list.add(a[i]);
+        }
+        System.out.println(list.findMax());
+    }
+}
+
+class Generic2<T extends Comparable> {
+
+    private ArrayList<T> array;
+
+    public Generic2() {
+        this.array = new ArrayList<T>();
+    }
+
+    public void add(T a) {
+        this.array.add(a);
+    }
+
+    public T findMax() {
+        T max = array.get(0);
+        for (T i : array) {
+            if (i.compareTo(max) > 0) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
 }
